4de686a2fb523d0c0867bbb319fe1528c5286486
DIXITSATHWARA <djsathwara540@gmail.com>
2022-03-16 11:51:55 +0530

    first commit

---
+++ src/Login.js
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useForm } from "react-hook-form";
+import { setUser } from './Utils/Common';
+import "./LoginForm.css";
+
+function Login(props) {
+  
+  const { register, handleSubmit, errors } = useForm();
+
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  // handle button click of login form
+  const handleLogin = (data) => {
+    setError(null);
+    setLoading(true);
+    axios.post('http://192.168.0.105:1111/api/v1/auth/signin', data).then(response => {
+      setLoading(false);
+      setUser(response.data._token);
+      props.history.push('/dashboard');
+    }).catch(error => {
+      setLoading(false);
+      console.log("====",error.response);
+      if (error.response && error.response.status === 401) setError(error.response.data.error);
+      else setError("Something went wrong. Please try again later.");
+    });
+  }
+
+  return (
+    <div>
+      Login<br /><br />
+      {error && <><small style={{ color: 'red' }}>{error}</small><br /></>}<br />
+      <div className="login-form">
+      <form onSubmit={handleSubmit(handleLogin)} noValidate>
+        <label htmlFor="inputEmail">Username</label>
+        <input
+          type="email"
+          id="inputEmail"
+          name="emailAddress"
+          ref={register({
+            required: "Enter your username",
+            pattern: {
+              value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+              message: "Enter a valid email address",
+            },
+          })}
+        />
+        {errors.emailAddress && <p className="error">{errors.emailAddress.message}</p>}
+
+        <label htmlFor="inputPassword">Password</label>
+        <input
+          type="password"
+          id="inputPassword"
+          name="password"
+          ref={register({ required: "Enter your password" })}
+        />
+        {errors.password && <p className="error">{errors.password.message}</p>}
+
+        <button type="submit">Login</button>
+      </form>
+    </div>
+    </div>
+  );
+}
+
+
+export default Login;
